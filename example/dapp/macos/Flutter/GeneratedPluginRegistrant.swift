47bcd2cf09bd36ea9cfc169e3fba077a7129e39d
chetan-cueclad <chetan.gairwar@cueclad.com>
2024-05-10 19:43:24 +0530

    Plugins are updated.

--- example/dapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/dapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import package_info_plus
-import shared_preferences_foundation
+import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
